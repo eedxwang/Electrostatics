3e93e29c2ab6b91f6c45df960baf6438ca064cf3
knordstrom <1003412n@student.gla.ac.uk>
2013-03-20 12:46:32 +0000

    Added some commented out attempts to further optimise FFD. Changed bmpshape.cc.

--- code/Algorithms/Fast_Finite_Difference.cpp
+++ code/Algorithms/Fast_Finite_Difference.cpp
@@ -11,6 +11,7 @@ void Fast_Finite_Difference::solve(){
 	current = &one;
 	alternate = &two;
 	bool error = 1;
+	double err = 0.;
 	double betaSq = beta*beta;
 	double divisor = 2*(1+betaSq);
 	int n = current->size()-1;
@@ -22,12 +23,14 @@ void Fast_Finite_Difference::solve(){
 		alternate = temp;
 		for (int x = 1; x < n; x++) {
 			for (int y = 1; y < m; y++) {
-				if (!(*current)[x][y].boundary) {
+				if (!(*current)[x][y].boundary) { //&& (*current)[x][y].flag != 5) {
 					(*alternate)[x][y].value = (
 							(*current)[x-1][y].value + (*current)[x+1][y].value +
 							betaSq*((*current)[x][y-1].value + (*current)[x][y+1].value))/divisor;
-					if (!error)
-						error = std::abs((*alternate)[x][y].value - (*current)[x][y].value) > precision;
+                    //err = std::abs((*alternate)[x][y].value - (*current)[x][y].value);
+                    if (!error)error = std::abs((*alternate)[x][y].value - (*current)[x][y].value) > precision;
+                    //if(err < precision) (*alternate)[x][y].flag = 5;
+                    //if(err > precision) {(*alternate)[x-1][y].flag = 0; (*alternate)[x+1][y].flag = 0; (*alternate)[x][y-1].flag = 0; (*alternate)[x][y+1].flag = 0;}
 				}
 			}
 		}
--- programs/bmpshape.cc
+++ programs/bmpshape.cc
@@ -5,6 +5,7 @@
 #include "Errors/Compare.h"
 #include "Utils/Conductors.h"
 #include <ctime>
+#include <unistd.h>
 #include "Utils/BmpReader.h"
 
 using namespace std;
@@ -12,8 +13,8 @@ using namespace std;
 int main() {
 
 	int N = 10 ;     // number of eq.p. lines
-	int n = 999;
-	int m = 999;
+	int n = 499;
+	int m = 499;
 
 
 	double Emax=50;
@@ -21,24 +22,29 @@ int main() {
 
 	Bmp_Reader bmp("shape.bmp");
 	Grid first_grid = bmp.get_grid(conductor,Emax,Emin,10.);
-	cout << "got shape" << endl;
+	cout << "Shape parsed." << endl;
 
 	//first_grid.set_flow_careful(Emax, Emin);
+
 
 	Fast_Finite_Difference fd (first_grid);
 	fd.set_precision(0.00001);
 	fd.set_maxit(10000);
+	cout << "Solving..." << endl;
+	//timespec t0, t1;
+	//clock_gettime(CLOCK_REALTIME, &t0);
     Grid sol = fd.get_solution();
-    cout << "done solving" << endl;
+    //clock_gettime(CLOCK_REALTIME, &t1);
+    //cout << difftime(t1.tv_sec,t0.tv_sec) << endl;
+    cout << "Done solving!" << endl;
+
+    cout << "Plotting..." << endl;
     sol.set_figure_outline();
 	sol.print_all_to("results.dat");
-
-
 
 	Gnuplot gp(sol);
 	sol.equip_values(N,n,m,Emax,1,1);
 	sol.print_contours_to("eq_lines.dat", N);
-    cout << "done calling lines" << endl;
 
 	gp.add_command("set term postscript");
     gp.add_command("set ytics ('1000' 999)");
@@ -47,8 +53,11 @@ int main() {
 	gp.add_command("set palette defined");
     gp.add_command("unset key");
     //gp.add_plot();
-	gp.add_command("plot 'results.dat' with image, 'eq_lines.dat' with lines ls -1"); //, 'figure.dat' with lines ls -1"); // white = ls -3; black = ls -1; red = ls 1; green = 2; blue = ls 3; violet = ls 4;
+	gp.add_command("plot 'results.dat' with image, 'eq_lines.dat' with lines ls -1");
+	//, 'figure.dat' with lines ls -1"); // white = ls -3; black = ls -1; red = ls 1; green = 2; blue = ls 3; violet = ls 4;
 	gp.sendString();
+
+    cout << "Done plotting!" << endl;
 
 	return 0;
 
